74fdc40d52ceedab444e0d31718dabee3480caeb
0xnightwind <nightwind1899@gmail.com>
2025-09-29 08:25:56 +0000

    update basics
    Signed-off-by: 0xnightwind <nightwind1899@gmail.com>

--- basics.sh
+++ basics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+apt update && apt upgrade -y
 apt install sudo -y
-sudo apt update && sudo apt upgrade -y
 sudo apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev lsof iproute2 g++ -y
 sudo apt install python3 python3-pip python3-venv python3-dev -y
 sudo apt update
